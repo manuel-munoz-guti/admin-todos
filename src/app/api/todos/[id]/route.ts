f979156a0de647480038c47b874344b940e156ae
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2024-02-07 21:53:08 -0400

    Adding endpoints for todo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 interface Segments {
     params: {
@@ -7,12 +9,19 @@ interface Segments {
     }
 }
 
-export async function GET(request: Request, segments: Segments) { 
-  const { params: { id } } = segments;
-
+const getTodo = async( id: string ): Promise<Todo | null> => {
+  
   const todo = await prisma.todo.findUnique({
     where: { id }
   });
+
+  return todo;
+}
+
+export async function GET(request: Request, segments: Segments) { 
+  const { params: { id } } = segments;
+
+  const todo = await getTodo(id);
 
   if (!todo) {
     return NextResponse.json(
@@ -24,4 +33,37 @@ export async function GET(request: Request, segments: Segments) {
   return NextResponse.json({
     todo
   })
+}
+
+
+const putSchema = yup.object({
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
+});
+
+export async function PUT(request: Request, segments: Segments) {
+  const { params: { id } } = segments;
+
+  try {
+    const todo = await getTodo(id);
+  
+    if (!todo) {
+      return NextResponse.json(
+          { message: 'Record not found'},
+          { status: 404 }
+      );
+    }
+  
+    const { complete, description } = await putSchema.validate( await request.json() );
+  
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description }
+    });
+  
+    return NextResponse.json(updatedTodo);
+
+  } catch (error) {
+    return NextResponse.json({ error, status: 400 });
+  }
 }
